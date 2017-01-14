b09e0483d41ba6083f0d839a988404ee5c2086a6
bklukaczewski <bklukaczewski@gmail.com>
2017-01-14 13:13:55 +0100

    Dynamic page indicator Component;

--- src/app/active-page-indicator/active-page-indicator.component.ts
+++ src/app/active-page-indicator/active-page-indicator.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
-  selector: 'active-page-indicator',
-  templateUrl: './active-page-indicator.component.html',
-  styleUrls: ['./active-page-indicator.component.scss']
+    selector: 'active-page-indicator',
+    templateUrl: './active-page-indicator.component.html',
+    styleUrls: ['./active-page-indicator.component.scss']
 })
 export class ActivePageIndicatorComponent implements OnInit {
+    @Input() elements: number;
+    @Input() active: number;
+    indicators: boolean[] = [];
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        for (let i = 1; i <= this.elements; i++) {
+            this.indicators.push(this.active === i);
+        }
+    }
 }
