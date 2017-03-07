8e93aadc59f04094e3ecf33aed5c65a26457a672
bklukaczewski <bklukaczewski@gmail.com>
2017-03-07 22:59:58 +0100

    Responsive tweaks; Changes to result screen;

--- client/src/app/results/results.component.ts
+++ client/src/app/results/results.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, HostBinding} from '@angular/core';
 import {slideAnimation} from '../app.routes.animations';
 import {BalanceService} from "../balance.service";
+import {DatePipe} from "@angular/common";
 
 @Component({
     selector: 'app-results',
@@ -12,10 +13,24 @@ import {BalanceService} from "../balance.service";
 export class ResultsComponent implements OnInit {
     @HostBinding('@routeAnimation') routeAnimation = true;
 
+    private datePipe: DatePipe;
     balance: string;
+    dateUpdated: any;
 
     constructor(private balanceService: BalanceService) {
         this.balance = balanceService.getBalanceAsUSD();
+        this.datePipe = new DatePipe('en-US');
+        this.dateUpdated = {
+            dateUpdated: this.datePipe.transform(this.getLastUpdateDate(), 'dd/MM/yyyy')
+        };
+    }
+
+    getLastUpdateDate() {
+        let today = new Date();
+        if (today.getHours() >= 12) {
+            return Date.now();
+        }
+        return Date.now() - 24*60*60*1000;
     }
 
     ngOnInit() {
